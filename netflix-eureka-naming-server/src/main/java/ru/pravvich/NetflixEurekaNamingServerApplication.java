dcf30710aaac1262b6499588579f60f6d1c66bb2
Pavel Ravvich <triodjangopiter@yandex.ru>
2019-03-12 01:49:04 +0300

    Add base configurations to eureka naming server and connect consumers

--- netflix-eureka-naming-server/src/main/java/ru/pravvich/NetflixEurekaNamingServerApplication.java
+++ netflix-eureka-naming-server/src/main/java/ru/pravvich/NetflixEurekaNamingServerApplication.java
@@ -1,9 +1,11 @@
-package ru.pravvich.netflixeurekanamingserver;
+package ru.pravvich;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NetflixEurekaNamingServerApplication {
 
 	public static void main(String[] args) {
--- netflix-eureka-naming-server/src/test/java/ru/pravvich/NetflixEurekaNamingServerApplicationTests.java
+++ netflix-eureka-naming-server/src/test/java/ru/pravvich/NetflixEurekaNamingServerApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.pravvich.netflixeurekanamingserver;
+package ru.pravvich;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
